9e350d9d825976e78cb779dfd47940979d2e5908
golusaini205 <124417338+golusaini205@users.noreply.github.com>
2026-02-02 15:09:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 require("dotenv").config();
 
 // Create app FIRST
@@ -15,10 +14,6 @@ app.use(express.json());
 const authRoutes = require("./routes/auth");
 const watchlistRoutes = require("./routes/watchlist");
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 // API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/watchlist", watchlistRoutes);
